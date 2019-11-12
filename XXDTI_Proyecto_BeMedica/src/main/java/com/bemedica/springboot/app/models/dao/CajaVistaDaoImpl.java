ccc86f5da274a37577015e81b4ae53ab221aac73
chrisca1209 <chris.cante2016@gmail.com>
2019-11-12 17:57:43 -0600

    Cambios extra

--- XXDTI_Proyecto_BeMedica/src/main/java/com/bemedica/springboot/app/models/dao/CajaVistaDaoImpl.java
+++ XXDTI_Proyecto_BeMedica/src/main/java/com/bemedica/springboot/app/models/dao/CajaVistaDaoImpl.java
@@ -7,7 +7,6 @@ import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import com.bemedica.springboot.app.models.entity.CajaVista;
 
 
 @Repository
