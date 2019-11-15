c5a911342fd2c96159f26066ffe418c0537654ab
Uriel Cuatepitzi Flores <warriorx72@gmail.com>
2019-11-15 13:12:16 -0600

    ok

--- XXDTI_Proyecto_BeMedica/src/main/java/com/bemedica/springboot/app/models/entity/Factura.java
+++ XXDTI_Proyecto_BeMedica/src/main/java/com/bemedica/springboot/app/models/entity/Factura.java
@@ -20,7 +20,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 //import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-//import org.springframework.format.annotation.DateTimeFormat;
+//import org.springframework.format.annotation.DateTimeFormat; listo
 
 @Entity
 @Table(name = "factura")
