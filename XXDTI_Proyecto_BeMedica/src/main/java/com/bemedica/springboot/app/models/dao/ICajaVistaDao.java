952a2ce87fc5937df14bc7bcb428bdd1801983c7
chrisca1209 <48454448+chrisca1209@users.noreply.github.com>
2019-11-28 11:39:43 -0600

    Add files via upload

--- XXDTI_Proyecto_BeMedica/src/main/java/com/bemedica/springboot/app/models/dao/ICajaVistaDao.java
+++ XXDTI_Proyecto_BeMedica/src/main/java/com/bemedica/springboot/app/models/dao/ICajaVistaDao.java
@@ -7,10 +7,12 @@ public interface ICajaVistaDao {
 
 	public List<Object []> findAll();
 	
-	public List<Object []> findAll2(int num1 , int num2);
+	public List<Object []> findAll2(int num1 , int num2, int num3);
 	
-	public float findAll3(int num1 , int num2);
+	public float findAll3(int num1,int num2);
+	
+	public List<Object []> findAll5(int num1, int num2,int num3);
 	
 	public List<Object []> findAll4(int num1 , int num2, int num3);
-	
+
 }
