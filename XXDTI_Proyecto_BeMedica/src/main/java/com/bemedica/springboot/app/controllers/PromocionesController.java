757514516c1797e6d3096ad1483afd2b49314341
TIErik <zemp.erik@gmail.com>
2019-12-06 12:34:13 -0600

    yaaaa pude wooooo que pinche emocion!!!!!

--- XXDTI_Proyecto_BeMedica/src/main/java/com/bemedica/springboot/app/controllers/PromocionesController.java
+++ XXDTI_Proyecto_BeMedica/src/main/java/com/bemedica/springboot/app/controllers/PromocionesController.java
@@ -8,11 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.bemedica.springboot.app.models.dao.IPromocionesDao;
+import com.bemedica.springboot.app.models.entity.DireccionE;
+import com.bemedica.springboot.app.models.entity.EmpleadosSucursal;
+import com.bemedica.springboot.app.models.entity.PersonaE;
 import com.bemedica.springboot.app.models.entity.Promociones;
 
 
@@ -35,19 +40,31 @@ public class PromocionesController {
 	@RequestMapping(value= "/form_promo", method=RequestMethod.POST)
 	public String guardarPromo (Map<String, Object> m,Promociones promo,BindingResult result , Model model,SessionStatus status)
 	{		
+		promo.setPromocionIdText("");
 		
-		if(result.hasErrors()) {
-			return "precios_promociones";
-		}
 		promocionesDao.save(promo);
 		if(promo.getPromocionIdText()=="") {
 			char buf[] = new char[3];
 			promo.getPromocionNombre().getChars(0,3,buf,0);
 			String IdText = String.valueOf(buf);
-			promo.setPromocionIdText(IdText.toLowerCase()+(promo.getPromocionId()+10000));
+			promo.setPromocionIdText(IdText.toLowerCase()+""+(promo.getPromocionId()+10000));
 		}
 		promocionesDao.save(promo);
 
-		return "precios_promociones";
+		return "redirect:/precios_promociones";
+	}
+	
+	
+	@RequestMapping (value="/eliminar_pro/{id}")
+	public String eliminar(@PathVariable (value="id") Long id  , RedirectAttributes redirectAttrs) {
+		if (id > 0 )
+		{
+			promocionesDao.delete(id);
+			redirectAttrs
+            .addFlashAttribute("mensaje", "Empleado eliminado correctamente")
+            .addFlashAttribute("clase", "success");
+		}
+		
+		return "redirect:/precios_promociones";
 	}
 }
