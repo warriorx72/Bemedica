002bcc653b8ea879da48a07fad02a07e425c5600
warriorx72 <39719764+warriorx72@users.noreply.github.com>
2020-01-15 16:29:18 -0600

    caja chica dao

---
+++ XXDTI_Proyecto_BeMedica/src/main/java/com/bemedica/springboot/app/models/dao/CajaChicaDaoImpl.java
@@ -0,0 +1,58 @@
+package com.bemedica.springboot.app.models.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bemedica.springboot.app.models.entity.CajaChica;
+@Repository
+public class CajaChicaDaoImpl implements ICajaChicaDao {
+	@PersistenceContext
+	private EntityManager em;
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	@Transactional(readOnly =true)
+	public List<CajaChica> findAll() {
+		// TODO Auto-generated method stub
+		return  em.createQuery("From CajaChica").getResultList();
+	}
+
+	@Override
+	@Transactional
+	public void save(CajaChica cajaChica) {
+		// TODO Auto-generated method stub
+		if(cajaChica.getCajaChicaId() != null && cajaChica.getCajaChicaId() >0) {
+			em.merge(cajaChica);
+		}else {
+			em.persist(cajaChica);	
+		}
+	}
+
+	@Override
+	@Transactional
+	public CajaChica findOne(Long id) {
+		// TODO Auto-generated method stub
+		return em.find(CajaChica.class, id);
+	}
+	@Transactional
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		em.remove(findOne(id));
+	}
+	@Transactional
+	@Override
+	public String findAiCaja() {
+		// TODO Auto-generated method stub
+		return em.createNativeQuery("SELECT AUTO_INCREMENT \r\n" + 
+				"FROM INFORMATION_SCHEMA.TABLES\r\n" + 
+				"WHERE TABLE_NAME='caja'\r\n" + 
+				"AND TABLE_SCHEMA=DATABASE()").getSingleResult().toString();
+	}
+
+}
---
+++ XXDTI_Proyecto_BeMedica/src/main/java/com/bemedica/springboot/app/models/dao/ICajaChicaDao.java
@@ -0,0 +1,19 @@
+package com.bemedica.springboot.app.models.dao;
+
+import java.util.List;
+
+import com.bemedica.springboot.app.models.entity.CajaChica;
+
+public interface ICajaChicaDao {
+	
+	public List<CajaChica> findAll();	
+	
+	public String findAiCaja();
+	
+	public void save (CajaChica cajaChica);
+	
+	public CajaChica findOne (Long id );
+	
+	public void delete (Long id);
+
+}
