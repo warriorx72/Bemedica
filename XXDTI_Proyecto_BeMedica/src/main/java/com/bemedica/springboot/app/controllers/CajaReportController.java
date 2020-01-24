b6009ee1ed849c9c78fe9bac9f38bd1f239e5bfe
CoDorsa <59836826+CoDorsa@users.noreply.github.com>
2020-01-24 16:14:55 -0600

    Update CajaReportController.java

--- XXDTI_Proyecto_BeMedica/src/main/java/com/bemedica/springboot/app/controllers/CajaReportController.java
+++ XXDTI_Proyecto_BeMedica/src/main/java/com/bemedica/springboot/app/controllers/CajaReportController.java
@@ -1,61 +1,43 @@
 package com.bemedica.springboot.app.controllers;
 
+//librerias de pdf
+import java.awt.Color;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
-import java.sql.Date;
-import java.text.SimpleDateFormat;
 import java.util.List;
-import java.util.Map;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
+import javax.swing.JComponent;
+import javax.swing.JTextField;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.bemedica.springboot.app.models.dao.ICajaVistaDao;
-import com.bemedica.springboot.app.models.entity.Promociones;
-import com.lowagie.text.Anchor;
-import com.lowagie.text.BadElementException;
-import com.lowagie.text.Chapter;
 import com.lowagie.text.Chunk;
 import com.lowagie.text.Document;
-import com.lowagie.text.DocumentException;
 import com.lowagie.text.Element;
-import com.lowagie.text.ElementTags;
 import com.lowagie.text.Font;
-import com.lowagie.text.FontFactory;
-import com.lowagie.text.Image;
-
-import com.lowagie.text.ListItem;
 import com.lowagie.text.Paragraph;
-import com.lowagie.text.Phrase;
 import com.lowagie.text.Rectangle;
-import com.lowagie.text.Section;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
-import com.lowagie.text.pdf.PdfWriter;
-import com.lowagie.text.pdf.draw.DottedLineSeparator;
+import com.lowagie.text.pdf.PdfWriter; 
 
-import java.awt.Color;
-import java.io.*;
-//librerias de pdf
+
+
 
 @Controller
+
 public class CajaReportController {
+	private static final Color RED = null;
+
 	@Autowired
 	private ServletContext context;
 
@@ -96,6 +78,7 @@ public class CajaReportController {
 	 * @param f1
 	 * @param f2
 	 * @param p
+	 * @return 
 	 * @throws Exception
 	 */
 	/*
@@ -180,7 +163,8 @@ public class CajaReportController {
 //			}
 //		}
 //	}
-
+	 
+	
 	private void filedownloadPDF(String fullPath, HttpServletResponse response) {
 		File file = new File(fullPath);
 		final int BUFFER_SIZE = 4096;
@@ -208,6 +192,7 @@ public class CajaReportController {
 		}
 	}
 
+	
 	@SuppressWarnings("null")
 	public void pdf(HttpServletRequest request, int num1) throws Exception {
 
@@ -220,9 +205,11 @@ public class CajaReportController {
 
 		// Se asocia el documento al OutputStream y se indica que el espaciado entre
 		// lineas sera de 20. Esta llamada debe hacerse antes de abrir el documento
-		Font iTextFont = new Font(Font.TIMES_ROMAN, 9, Font.NORMAL);
+		Font iTextFont = new Font(Font.TIMES_ROMAN, 9);
 		PdfWriter.getInstance(documento, ficheroPdf).setInitialLeading(20);
-
+		
+		
+        
 		// Se abre el documento.
 		documento.open();
 //		int edad=35;
@@ -236,9 +223,11 @@ public class CajaReportController {
 		List<Object[]> uc = cajaVistaDao.findAll3(num1);
 		List<Object[]> re = cajaVistaDao.findAll(); //para hacer el reporte
 		
-		Paragraph p = new Paragraph("\n\n\n\n\n\n");
+		Paragraph p = new Paragraph("\n\n");
 		PdfPCell celda1 = new PdfPCell(p);
 		celda1.setBorder(Rectangle.NO_BORDER);
+		celda1.setHorizontalAlignment(Element.ALIGN_LEFT);
+		
 		
 		/*********************************************************************************/
 
@@ -249,48 +238,142 @@ public class CajaReportController {
 
 		/*********************************************************************************/
 
+		Chunk yu = new Chunk("", new Font(
+				Font.HELVETICA, 12, Font.NORMAL, Color.BLACK));
+		Chunk mi = new Chunk("", new Font(
+				Font.HELVETICA, 12, Font.NORMAL, Color.BLACK));
+		
+		
+		
 		PdfPTable table3 = new PdfPTable(2);
 		table3.getDefaultCell().setBorder(0);
 		table3.setWidthPercentage(100);
-		table3.setWidths(new float[] { 2.0f, 1.8f });
+		table3.setWidths(new float[] { 2.0f, 2.0f });
 		table3.setSpacingBefore(2);
-		table3.addCell("Nombre: \n" + "Sucursal:");
-		table3.addCell("Empleado: \n" + "Fecha: " + fc);
+		yu.append("Nombre:\n" + "Sucursal:\n");
+		mi.append("Empleado:\n" + "Fecha:\t\t" + fc);
 
 		PdfPCell celda2 = new PdfPCell(table3);
 		celda2.setHorizontalAlignment(Element.ALIGN_LEFT);
 		celda2.setBorder(Rectangle.NO_BORDER);
+		
+		documento.add(yu);
+		documento.add(mi);
+		
 		/**************************************************************************/
+		
 
-		PdfPTable table = new PdfPTable(7);
+		
+		PdfPTable table = new PdfPTable(8);
 		table.getDefaultCell().setBorder(0);
-		table.setWidthPercentage(90.0f);
-		table.setWidths(new float[] { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f });
-		table.setSpacingBefore(2);
-		table.addCell("\n\n\nFolio");
-		table.addCell("\n\n\nUsuario");
-		table.addCell("\n\n\nMonto");
-		table.addCell("\n\n\nTotal");
-		table.addCell("\n\n\nFecha");
-		table.addCell("\n\n\nConcepto");
-		table.addCell("\n\n\nForma de pago\n\n");
-
+		table.setWidthPercentage(100);
+		table.setWidths(new float[] { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f});
+		table.setSpacingBefore(1);
+		Paragraph columna1 = new Paragraph("Folio");
+	    columna1.getFont().setStyle(Font.BOLD);
+	    columna1.getFont().setSize(12);
+	    table.addCell(columna1);
+	    Paragraph columna2 = new Paragraph("Usuario");
+	    columna2.getFont().setStyle(Font.BOLD);
+	    columna2.getFont().setSize(12);
+	    table.addCell(columna2);
+	    Paragraph columna3 = new Paragraph("Monto");
+	    columna3.getFont().setStyle(Font.BOLD);
+	    columna3.getFont().setSize(12);
+	    table.addCell(columna3);
+	    Paragraph columna4 = new Paragraph("Total");
+	    columna4.getFont().setStyle(Font.BOLD);
+	    columna4.getFont().setSize(12);
+	    table.addCell(columna4);
+	    Paragraph columna101 = new Paragraph("Resta");
+	    columna101.getFont().setStyle(Font.BOLD);
+	    columna101.getFont().setSize(12);
+	    table.addCell(columna101);
+	    Paragraph columna5 = new Paragraph("Fecha");
+	    columna5.getFont().setStyle(Font.BOLD);
+	    columna5.getFont().setSize(12);
+	    table.addCell(columna5);
+	    Paragraph columna6 = new Paragraph("Concepto");
+	    columna6.getFont().setStyle(Font.BOLD);
+	    columna6.getFont().setSize(12);
+	    table.addCell(columna6);
+	    Paragraph columna7 = new Paragraph("Forma\tde\tPago");
+	    columna7.getFont().setStyle(Font.BOLD);
+	    columna7.getFont().setSize(12);
+	    table.addCell(columna7);
+	   
+	    
+		//table.addCell("\nFolio");
+		//table.addCell("\nUsuario");
+		//table.addCell("\nMonto");
+		//table.addCell("\nTotal");
+		//table.addCell("\nFecha");
+		//table.addCell("\nConcepto");
+		//table.addCell("\nForma\tde\tpago\n\n");
+	  
+	   
 		for (Object[] a : rp) {
-
-			table.addCell(a[0].toString());
-			table.addCell(a[1].toString());
-			table.addCell(a[2].toString());
-			table.addCell(a[3].toString());
-			table.addCell(a[4].toString());
-			table.addCell(a[5].toString());
-			table.addCell(a[6].toString());
+			
+			  String num5 = a[2].toString();
+			  String num6 = a[3].toString();  //convertir objeto a variable string
+				
+				float n4 = Float.parseFloat (num5);
+				float n5 = Float.parseFloat (num6); //variable string a float
+				
+				float resta = (n4-n5); 
+			
+			
+			Paragraph columna8 = new Paragraph(a[0].toString());
+			columna8.getFont().setStyle(Font.NORMAL);
+			columna8.getFont().setSize(10);
+			table.addCell(columna8);
+			Paragraph columna9 = new Paragraph(a[1].toString());
+			columna9.getFont().setStyle(Font.NORMAL);
+			columna9.getFont().setSize(10);
+			table.addCell(columna9);
+			Paragraph columna10 = new Paragraph(a[2].toString());
+			columna10.getFont().setStyle(Font.NORMAL);
+			columna10.getFont().setSize(10);
+			table.addCell(columna10);
+			Paragraph columna11 = new Paragraph(a[3].toString());
+			columna11.getFont().setStyle(Font.NORMAL);
+			columna11.getFont().setSize(10);
+			table.addCell(columna11);
+			Paragraph columna121 = new Paragraph("" + resta);
+			columna121.getFont().setStyle(Font.NORMAL);
+			columna121.getFont().setSize(10);
+			table.addCell(columna121);
+			Paragraph columna12 = new Paragraph(a[4].toString());
+			columna12.getFont().setStyle(Font.NORMAL);
+			columna12.getFont().setSize(10);
+			table.addCell(columna12);
+			Paragraph columna13 = new Paragraph(a[5].toString());
+			columna13.getFont().setStyle(Font.NORMAL);
+			columna13.getFont().setSize(10);
+			table.addCell(columna13);
+			Paragraph columna14 = new Paragraph(a[6].toString());
+			columna14.getFont().setStyle(Font.NORMAL);
+			columna14.getFont().setSize(10);
+			table.addCell(columna14);
+			
+			//table.addCell(a[0].toString());
+			//table.addCell(a[1].toString());
+			//table.addCell(a[2].toString());
+			//table.addCell(a[3].toString());
+			//table.addCell(a[4].toString());
+			//table.addCell(a[5].toString());
+			//table.addCell(a[6].toString());
+			
 		}
-
+		
+		
 		/**************************************************************************/
 
 		PdfPTable table4 = new PdfPTable(1);
 		table4.getDefaultCell().setBorder(0);
 		table4.addCell(celda2);
+		
+		
 
 		/**************************************************************************/
 
@@ -312,32 +395,95 @@ public class CajaReportController {
 					//+"\nMonto \nEfectivo:  $"+b[3].toString());
 			//table1.addCell("\nTotal:      $" + b[4].toString());
 	//	}
+		Chunk ck = new Chunk("", new Font(
+				Font.HELVETICA, 12, Font.NORMAL, Color.BLACK));
+		Chunk no = new Chunk("", new Font(
+				Font.HELVETICA, 12, Font.NORMAL, Color.BLACK));
+		Chunk si = new Chunk("", new Font(
+				Font.HELVETICA, 12, Font.NORMAL, Color.BLACK));
+		Chunk tu = new Chunk("", new Font(
+				Font.HELVETICA, 12, Font.NORMAL, Color.BLACK));	
+		Chunk pa = new Chunk("", new Font(
+				Font.HELVETICA, 12, Font.NORMAL, Color.BLACK));
+		Chunk ma = new Chunk("", new Font(
+				Font.HELVETICA, 12, Font.NORMAL, Color.BLACK));
+		Chunk du = new Chunk("", new Font(
+				Font.HELVETICA, 12, Font.NORMAL, Color.BLACK));
+		Chunk os = new Chunk("", new Font(
+				Font.HELVETICA, 12, Font.NORMAL));
+		Chunk pi = new Chunk("", new Font(
+				Font.HELVETICA, 12, Font.NORMAL, Color.BLACK));
+		
+		
 		for(Object[] r : re) 
+			
 		{
+			
+			
 			//System.out.println("szfxdgchj"+r[6]+" "+r[0]+" "+Integer.toString(num1));
 		if (r[0].equals(num1)&&r[6].equals("Corte"))
 		{
-			//System.out.println("entra1");
-				table1.addCell("\n\n\n\n\nTotal de\nEfectivo:      $" +r[8].toString()+"\n"
-						+"\nTotal \nTarjeta:  $" +r[9].toString()+"\n"
-							+"\nCaja \nChica:       $"+r[10].toString()+"\n"
-						+"\nMonto \nEfectivo:  $"+r[7].toString());
-				table1.addCell("\nTotal:      $" + r[4].toString());
+
 			
-		} else if(r[0].equals(num1)&&r[6].equals("Cierre"))
+			String num2 = r[8].toString();
+			String num3 = r[10].toString();  //convertir objeto a variable string
+			String num4 = r[7].toString();
+			
+			float n1 = Float.parseFloat (num2);
+			float n2 = Float.parseFloat (num3); //variable string a float
+			float n3 = Float.parseFloat (num4);
+			
+			float suma = ((n3)-(n1+n2)); //variable ya flotante hacer la operación para mostrar nuevo campo
+			
+			if(suma==0) {
+				os.setFont(new Font(
+				Font.HELVETICA, 12, Font.NORMAL, Color.BLACK));
+			}
+			else if(suma>0){
+				os.setFont(new Font(
+				Font.HELVETICA, 12, Font.NORMAL, Color.green));
+			}
+			else if(suma<0) {
+				os.setFont(new Font(
+				Font.HELVETICA, 12, Font.NORMAL, Color.red));
+			}
+			
+			//System.out.println("entra1");
+			tu.append("\nTotal\tEfectivo:  $" +r[8].toString());
+			pa.append("\nTotal\tTarjeta:  $" +r[9].toString());
+			ma.append("\nCaja\tChica:  $"+r[10].toString());
+			du.append("\nMonto\tEfectivo: $"+r[7].toString());
+			os.append("\nDiferencia: $"+suma); //concatenar nuevo campo asignado a una  variable ya convertida
+		    pi.append("\nTotal: $" + r[4].toString());
+
+		} 
+		
+		
+		else if(r[0].equals(num1)&&r[6].equals("Cierre"))
 		{
 			//System.out.println("entra2");
-					table1.addCell("\n\n\n\n\nTotal de\nEfectivo:      $" +r[8].toString()+"\n"
-							+"\nTotal \nTarjeta:  $" +r[9].toString());
-					table1.addCell("\nTotal:      $" + r[4].toString());
+			 ck.append("\nTotal\tEfectivo: $" +r[8].toString());
+			 no.append("\nTotal\tTarjeta:  $" +r[9].toString());
+			 si.append("\nTotal: $" + r[4].toString());
 		}
 		}
+		
 		/**************************************************************************/
-
+		
 		documento.add(table5);
 		documento.add(table4);
 		documento.add(table);
 		documento.add(table1);
+		documento.add(ck);
+		documento.add(no);
+		documento.add(si);
+		documento.add(tu);
+		documento.add(pa);
+		documento.add(ma);
+		documento.add(du);
+		documento.add(os);
+		documento.add(pi);
+		
 		
 
 		documento.close();
@@ -345,5 +491,14 @@ public class CajaReportController {
 		System.out.println(fullPath);
 	
 		}
+
+	private void setForeground() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initComponents() {
+		// TODO Auto-generated method stub
+		
+	}
 		}
-	
